e48735858952bff7bc5ff07172ac805586d55691
Richy Recarey <richyrecarey@gmail.com>
2018-06-03 04:38:56 +0200

    New commit

--- R/hello.R
+++ R/hello.R
@@ -1,19 +1,28 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+#' @export
 
-hello <- function() {
-  print("Hello, world man!")
-  x = 233
+hello <- function(x) {
+  y <- hello1(x)
+  y
 }
+
+#' Diferent function
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+
+# We will not export this one, it wont be loaded with the package
+# But we can use it with computations I think
+hello1 <- function(x) {
+y <- x^2
+y
+}
+
