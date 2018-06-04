c8865e47ab7b96ef89653e37ba5f713246ef00de
Richy Recarey <richyrecarey@gmail.com>
2018-06-04 18:47:22 +0200

    Add two functions, eliminate 2

---
+++ R/classifast.R
@@ -0,0 +1,86 @@
+#' Add together two numbers.
+#'
+#' @param x Data frame or matrix with multivariate data with n observations (rows) and p variables (cols)
+#' @param y A factor with the labels of the rows of x
+#' @return hola
+#' @examples hola
+#' @export
+
+classifast <- function(x, y, prob, type = "simple"){
+
+  # Change the input x and y accordingly
+
+  # Needed variables throughout the function:
+  n <- nrow(x)
+  p <- ncol(x)
+
+  # In case "y" is a data.frame, modify it accordingly
+  if(is.data.frame(y)){
+    y <- y[[1]]
+  }
+
+  # Change "y" to factor. If "y" was a factor inside the data.frame,
+  # y[[1]] will keep being a "factor", so:
+  if (!is.factor(y)){
+    y <- factor(y, levels = paste(unique(y)))
+  }
+
+  # Check for lengths
+  if(n != length(y)){
+    stop("The labels vector must have the same length as the number of rows in your data")
+  }
+
+  # If "x" is a matrix, we turn it into d.f.
+  if(class(x) == "matrix"){
+    x <- data.frame(x)
+  }
+
+  # Creation of several character vectors needed
+  # plus we change the names of the variables to V1, ..., Vp
+  b1 <- paste0("V", 1:p,collapse = "+")
+  b2 <- paste0("V", 1:p)
+  # This formula y~V1+...+Vp will be usefull for some
+  # of the classifiers
+  formula <- paste("y ~ ",b1,sep = "")
+
+  colnames(x) <- b2
+
+  # We add the factor "y" to the data.frame "x"
+  x$y <- y
+
+  # At this point, we should have a dataframe "x" with p+1 cols
+  # and "y" in the last one as a factor
+
+  # Selection of train and test set
+  # on the "x" data.frame, keeping "y" the whole time.
+  train.n <- floor(prob * n)
+  # Indexex for the train set
+  train.i <- sample(n, train.n)
+  # Indexes for the test set
+  test.i <- c(1:n)[-train.i]
+
+  # New sets, keeping the factor "y" of labels in them
+  # For each, we take the rows "train.i" and "test.i"
+  # This 2 data.frames will be given to all the subfunctions
+  # This way we do the subsetting only once, in the main function
+  x.train <- x[train.i, ]
+  x.test <- x[test.i, ]
+
+  # ------------------------- MODELS ------------------------------ #
+  return(list(x = x.train, y = x.test))
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
